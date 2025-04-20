8ca8f8cf5012fed55ebcb49bfe94797595dbef01
eldad <eldadi9@gmail.com>
2025-04-21 02:16:51 +0300

    channel keywoed fixed more options

--- channel_keywords.py
+++ channel_keywords.py
@@ -12,7 +12,7 @@ CATEGORY_KEYWORDS_EN = {
         'KAN 11 HD [IL]', 'KAN 11 [IL]', 'Makan 33 HD', 'Kan Chinuchit 23', 'i24 News', 'Channel 9',
         'Now 14 HD [IL]', 'Mikan 33 [IL]', 'Kabbalah channel [IL]', 'KAN23 [IL]', 'Kan Educational HD [IL]',
         'Channel 24', 'Channel 98 IL', 'CHANNEL 12 HD IL', 'CHANNEL 13 HD', 'ערוץ 14', 'ערוץ 24', 'Kan 11 HD IL', 'Kan Educational IL', 'Keshet 12 FHD IL'
-        , 'Keshet 12 HD IL' , 'Reshet 13 HD IL'
+        , 'Keshet 12 HD IL' , 'Reshet 13 HD IL', 'IL: i24_News', 'IL: Hidabroot', 'IL: Hala TV', 'IL: Israel HD', 'IL: Kneest', 'IL: Kabbalah TV', 'IL: Makan HD'
     ],
     'Hot🔥': [
         'HOT', 'HOT CINEMA', 'HOT Cinema 1 HD IL', 'HOT CINEMA 1', 'HOT CINEMA 3', 'HOT CINEMA 4', 'HOT3 HD',
@@ -43,51 +43,75 @@ CATEGORY_KEYWORDS_EN = {
         'MMA-TV.com HD', 'NHL', 'nba', 'NBA', 'wwe', 'WWE Network HD'
     ],
     'Kids🍦': [
-        'Hop!', 'Israelit', 'Baby IL', 'Yaldut IL', 'BABY TV IL', 'hop', 'HOT A+ Kids', 'Nick Jr', 'Nickelodeon',
-        'Disney Junior', 'Luli', 'Junior', 'Disney HD', 'Baby', 'Hop! Childhood', 'Yaldut', 'Disney Jr [IL]',
-        'ZOOM', 'Disney Channel H', 'YoYo', 'NICK JR HD IL', 'Nick Jr IL', 'NICK HD IL', 'FOMO [IL]', 'HOP [IL]',
-        'Yoyo [IL]', 'Disney [IL]', 'Junior IL', 'hop IL', 'HOP HD IL', 'JUNIOR IL', 'Zoom', 'Zoom Toon HD',
-        'Wiz', 'Yalduti', 'TeenNick', 'Nick HD', 'Nick Jr HD', 'Logi', 'Jim Jam', 'Disney Jr.', 'LULI IL',
-        'Disney Jr IL', 'Baby TV', 'DISNEY JR IL', 'TeenNick IL', 'ZOOM IL', 'HOP CHILDHOOD IL', 'KIDS HD IL',
-        'Hop Israeli Childhood', 'Hop Pele HD', 'DISNEY CHANNEL', 'WIZ IL'
-    ],
+    'Hop!', 'Israelit', 'Baby IL', 'Yaldut IL', 'BABY TV IL', 'hop', 'HOT A+ Kids', 'Nick Jr', 'Nickelodeon',
+    'Disney Junior', 'Luli', 'Junior', 'Disney HD', 'Baby', 'Hop! Childhood', 'Yaldut', 'Disney Jr [IL]',
+    'ZOOM', 'Disney Channel H', 'YoYo', 'NICK JR HD IL', 'Nick Jr IL', 'NICK HD IL', 'FOMO [IL]', 'HOP [IL]',
+    'Yoyo [IL]', 'Disney [IL]', 'Junior IL', 'hop IL', 'HOP HD IL', 'JUNIOR IL', 'Zoom', 'Zoom Toon HD',
+    'Wiz', 'Yalduti', 'TeenNick', 'Nick HD', 'Nick Jr HD', 'Logi', 'Jim Jam', 'Disney Jr.', 'LULI IL',
+    'Disney Jr IL', 'Baby TV', 'DISNEY JR IL', 'TeenNick IL', 'ZOOM IL', 'HOP CHILDHOOD IL', 'KIDS HD IL',
+    'Hop Israeli Childhood', 'Hop Pele HD', 'DISNEY CHANNEL', 'WIZ IL', 'IL: A+ kids',
+    'Baby TV [UK]', 'Baby [IL]', 'Junior [IL]', 'Luli [IL]', 'Nick Jr [IL]', 'Nickelodeon [IL]',
+    'TeenNick [IL]', 'ZOOM TOON [IL]', 'ZOOM [IL]'
+]
+,
     'Entertainment🧸': [
-        'Home Plus IL', 'Good Life', 'FOOD CHANNE IL', '5Stars HD IL', 'Polsat HD', 'Food Channel', 'Ego Total',
-        'Health', 'EGO TOTAL HD IL', 'STARS IL', 'CANAL+ FAMILY HD PL', 'HISTORY HD IL', 'Star Channel',
-        'Reality HD', 'Savri HD', 'A+ HD IL', 'LIFETIME HD IL', 'STARS HD IL', 'Game Show Channel HD IL',
-        'E!', 'Horse and Country TV', 'ZONE HD', 'TLC HD', 'Love Island', 'History HD', 'Humor Channel',
-        'Fomo', 'Fashion', 'Holiday channel [IL]', 'Food Channel HD', 'Foody HD', 'Erez Nehederet HD',
-        'Big', 'CBS Reality', 'Boomerang', 'Entertainment IL', 'HEALTH CHANNEL', 'HUMOR CHANNEL', 'E! IL', 'Arutz Haofna Haisraeli HD IL'
-    ],
+    'Home Plus IL', 'Good Life', 'FOOD CHANNE IL', '5Stars HD IL', 'Polsat HD', 'Food Channel', 'Ego Total',
+    'Health', 'EGO TOTAL HD IL', 'STARS IL', 'CANAL+ FAMILY HD PL', 'HISTORY HD IL', 'Star Channel',
+    'Reality HD', 'Savri HD', 'A+ HD IL', 'LIFETIME HD IL', 'STARS HD IL', 'Game Show Channel HD IL',
+    'E!', 'Horse and Country TV', 'ZONE HD', 'TLC HD', 'Love Island', 'History HD', 'Humor Channel',
+    'Fomo', 'Fashion', 'Holiday channel [IL]', 'Food Channel HD', 'Foody HD', 'Erez Nehederet HD',
+    'Big', 'CBS Reality', 'Boomerang', 'Entertainment IL', 'HEALTH CHANNEL', 'HUMOR CHANNEL', 'E! IL',
+    'Arutz Haofna Haisraeli HD IL', 'IL: Extreme', 'IL: Humor Chanenel', 'IL: Stars',
+    'Big Ass Adult TV', 'Big Dick Red TV', 'Big Tits Adult TV', 'E! [IL]', 'Ego Total [IL]',
+    'Fashion TV', 'Foody HD [IL]', 'Good Life [IL]', 'Health channel [IL]', 'Humor Channel HD [IL]',
+    'Israeli Fashion [IL]', 'RedBull TV [UK]', 'Star channel [IL]', 'The Food Channel [IL]'
+],
+
     'Music🎵': [
-        'music', 'MUSIC', 'MUSIC 24', 'MTV Hits', 'MTV Base HD', 'Stingray ', 'MTV Hits', 'Stingray Hot Country HD',
-        'Stingray Pop Adult HD', 'Stingray Hit List HD', 'MTV Club', 'Clubbing TV HD', 'IL: MTV HD', 'MTV 80s',
-        'MTV Pulse HD', 'IT: MTV HD', 'MTV Idol HD', 'VH1 Classic', 'Rock Classics', 'Europa Plus TV HD',
-        'Music Box Gold', 'Club MTV', 'Bridge Deluxe HD', 'Bridge Hits', 'Bridge Rock', 'Europa Plus TV',
-        'MTV Live HD', 'MTV 90s', 'Yosso TV Music Hits', 'Fresh Concerts', 'Fresh Dance', 'Sky High Concert HD',
-        'Movistar Musica HD', '1HD Music Television orig', 'B4U Music IN', 'BOX Music 4K HDR', 'Backus TV Music HD',
-        'Baraza Music HD', 'Biz Music HD UZ', 'CHANNEL 24 MUSIC HD IL', 'Classic Music HD', 'Disco Polo Music PL',
-        'EU Music HD UA', 'FRESH Sad Music HD', 'KLI Music HD', 'MTV Aitio HD SE', 'MTV Base UK', 'MTV Classic',
-        'MTV Classic USA', 'MTV HD CA', 'MTV Music UK', 'MTV POLSKA PL', 'MTV RO', 'MTV Viihde HD FI',
-        'MUSIC 24 IL', 'Music Box Russia', 'Music Channel RO', 'MusicBox GE', 'Prokop TV Music', 'Public Music IN',
-        'Retro Music TV HD CZ', 'SUN Music IN', 'Sochi Music HD', 'VB MTV Old Россия HD', 'VF Music', 'Z!Music HD',
-        'ТНТ Music', 'ТНТ Music HD'
-    ],
+    'music', 'MUSIC', 'MUSIC 24', 'MTV Hits', 'MTV Base HD', 'Stingray ', 'MTV Hits', 'Stingray Hot Country HD',
+    'Stingray Pop Adult HD', 'Stingray Hit List HD', 'MTV Club', 'Clubbing TV HD', 'IL: MTV HD', 'MTV 80s',
+    'MTV Pulse HD', 'IT: MTV HD', 'MTV Idol HD', 'VH1 Classic', 'Rock Classics', 'Europa Plus TV HD',
+    'Music Box Gold', 'Club MTV', 'Bridge Deluxe HD', 'Bridge Hits', 'Bridge Rock', 'Europa Plus TV',
+    'MTV Live HD', 'MTV 90s', 'Yosso TV Music Hits', 'Fresh Concerts', 'Fresh Dance', 'Sky High Concert HD',
+    'Movistar Musica HD', '1HD Music Television orig', 'B4U Music IN', 'BOX Music 4K HDR', 'Backus TV Music HD',
+    'Baraza Music HD', 'Biz Music HD UZ', 'CHANNEL 24 MUSIC HD IL', 'Classic Music HD', 'Disco Polo Music PL',
+    'EU Music HD UA', 'FRESH Sad Music HD', 'KLI Music HD', 'MTV Aitio HD SE', 'MTV Base UK', 'MTV Classic',
+    'MTV Classic USA', 'MTV HD CA', 'MTV Music UK', 'MTV POLSKA PL', 'MTV RO', 'MTV Viihde HD FI',
+    'MUSIC 24 IL', 'Music Box Russia', 'Music Channel RO', 'MusicBox GE', 'Prokop TV Music', 'Public Music IN',
+    'Retro Music TV HD CZ', 'SUN Music IN', 'Sochi Music HD', 'VB MTV Old Россия HD', 'VF Music', 'Z!Music HD',
+    'ТНТ Music', 'ТНТ Music HD',
+    'BRIDGE TV Deluxe', 'BRIDGE TV HITS', 'BRIDGE TV Русский Хит', 'Bridge TV Classic', 'Bridge TV ROCK',
+    'C Music TV', 'Clarity4K Bollywood music', 'Clarity4K France music', 'Clarity4K Korean music',
+    'Clarity4K Latin music', 'Clarity4K Remastered music', 'Clarity4K Russian music', 'Clarity4K Ukrainian music',
+    'HIT MUSIC [RO]', 'ITV 2 [UK]', 'MTV', 'MTV 00s', 'MTV 00s [RO]', 'MTV 80s [RO]', 'MTV 90s [RO]',
+    'MTV Dance [RO]', 'MTV HD [DE]', 'MTV HD [UK]', 'MTV Hits [RO]', 'MTV Hits [UK]', 'MTV Live HD [PL]',
+    'MTV Live [UK]', 'MTV POLSKA [PL]', 'MTV [AZ]', 'MTV [IL]', 'Music Channel [IL]', 'VOX MUSIC TV [PL]',
+    'ТНТ MUSIC'
+],
+
     'Nature🌴': [
         'Discovery', 'Travel Channel', 'DISCOVERY CHANNEL HD IL', 'Nat Geo HD', 'Nat Geo Wild', 'Animal Planet HD',
         'National Geographic [IL]', 'NET GEO_WILD HD IL', 'Sky Select 5 HD', 'NAT GEO WILD IL', 'History [IL]',
         'IETV [IL]', 'ZEE TV [IL]', 'TRAVEL CHANNEL IL', 'Animal Planet [IL]', 'National Geographic HD [IL]',
-        'NATIONAL GEOGRAPHICS HD IL', 'National Geographic IL', 'National Geographic HD IL'
+        'NATIONAL GEOGRAPHICS HD IL', 'National Geographic IL', 'National Geographic HD IL', 'IL: Animal Planet',
+        'Discovery HD [IL]', 'Nat Geo Wild HD [IL]', 'Travel Channel [IL]'
     ],
+
     'World Series🌍': [
-        'Viva Premium HD IL', 'Turkish Dramas 3 HD IL', 'Turkish Dramas 2 HD IL', 'Turkish Dramas Plus HD IL',
-        'Viva', 'Yam Tihoni 25', 'Viva plus', 'Aruch Sdarot Hahodiot', 'Yam Tihoni Plus', 'Vamos HD',
-        'Yam Tihoni HD', 'Yam Tihoni 2', 'Viva+ IL', 'Viva+', 'Viva Vintage', 'Star channel [IL]',
-        'Mediterranean [IL]', 'Bollywood [IL]', 'Turkish Dramas Plus HD [IL]', 'VIVA IL', 'Yamtihoni IL',
-        'VIVA HD IL', 'VIVA+ IL', 'The Mediterranean HD [IL]', 'The Mediterranean [IL]',
-        'The Mediterranean+ HD [IL]', 'HALA TV IL', 'BOLLYWOOD HD IL', 'BOLLYSHOW HD IL', 'Bollywood HD', 'Turkish Dramas 2 FHD IL', 'Turkish Dramas 3 FHD IL', 'Turkish Dramas Plus FHD IL'
-        , 'Turkish Dramas Plus IL', 'Yam Tichoni HD IL', 'Yam Tichoni IL', 'Yam Tichoni+ HD IL'
-    ]
+    'Viva Premium HD IL', 'Turkish Dramas 3 HD IL', 'Turkish Dramas 2 HD IL', 'Turkish Dramas Plus HD IL',
+    'Viva', 'Yam Tihoni 25', 'Viva plus', 'Aruch Sdarot Hahodiot', 'Yam Tihoni Plus', 'Vamos HD',
+    'Yam Tihoni HD', 'Yam Tihoni 2', 'Viva+ IL', 'Viva+', 'Viva Vintage', 'Star channel [IL]',
+    'Mediterranean [IL]', 'Bollywood [IL]', 'Turkish Dramas Plus HD [IL]', 'VIVA IL', 'Yamtihoni IL',
+    'VIVA HD IL', 'VIVA+ IL', 'The Mediterranean HD [IL]', 'The Mediterranean [IL]',
+    'The Mediterranean+ HD [IL]', 'HALA TV IL', 'BOLLYWOOD HD IL', 'BOLLYSHOW HD IL', 'Bollywood HD',
+    'Turkish Dramas 2 FHD IL', 'Turkish Dramas 3 FHD IL', 'Turkish Dramas Plus FHD IL',
+    'Turkish Dramas Plus IL', 'Yam Tichoni HD IL', 'Yam Tichoni IL', 'Yam Tichoni+ HD IL',
+    'IL: Turkish Drama HD', 'IL: Turkish Drama 2',
+    'Bollywood HD [IL]', 'Turkish Dramas 2 HD [IL]', 'Turkish Dramas 2 [IL]',
+    'Turkish Dramas 3 HD [IL]', 'Turkish Dramas Plus [IL]', 'Viva Plus [IL]',
+    'Viva Premium HD [IL]', 'Viva Vintage [IL]', 'Viva [IL]'
+]
+
 }
 
 CATEGORY_KEYWORDS_HE = {
